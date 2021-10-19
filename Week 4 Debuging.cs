9f02cd66b2b0eb7760d850d833ccaca48a49f023
Ray <57572379+JustCallMeRay@users.noreply.github.com>
2021-10-19 18:31:00 +0100

    Create Week 4 Debuging.cs

---
+++ Week 4 Debuging.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugScript : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+		{
+            Debug.Log("You have pressed 'A'");
+
+		}
+        if (Input.GetKeyDown(KeyCode.Space))
+		{
+            Debug.Log("You have pressed Space Bar");
+		}
+    }
+}
