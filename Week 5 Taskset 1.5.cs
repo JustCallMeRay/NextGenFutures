938cafaf217751ca645cce2fe0601f973f5ade2e
Ray <57572379+JustCallMeRay@users.noreply.github.com>
2021-11-02 19:46:34 +0000

    Create Week 5 Taskset 1.5.cs

---
+++ Week 5 Taskset 1.5.cs
@@ -0,0 +1,34 @@
+/*
+start of Proc-gen explination, 
+not really a task. 
+*/
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessingAbout : MonoBehaviour //Direct quote. 
+{
+    void Start()
+    {
+        var mesh = GetComponent<MeshFilter>().mesh;
+
+        Vector3[] verts = mesh.vertices;
+
+        verts[1].y++; //? 
+
+        mesh.vertices = verts;
+
+
+        for (int i = 0; i < verts.Length; i++)
+		{
+            if(verts[i].y > 0)
+		      	{
+                 verts[i].y++; 
+			    }
+			}
+    }
+
+ 
+}
