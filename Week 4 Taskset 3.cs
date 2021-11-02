710d9a3344d6867f859eb2d2f99dabbf7f6c5a92
Ray <57572379+JustCallMeRay@users.noreply.github.com>
2021-11-02 19:23:29 +0000

    Update Week 4 Taskset 3.cs

--- Week 4 Taskset 3.cs
+++ Week 4 Taskset 3.cs
@@ -8,4 +8,7 @@ Extras:
 Create a makeshift menu (on clicked events)
 Make a combination lock
 import assests, add lighting 	
+
+I have done this (other than mouse down cuz that wasn't working) just need to find the code 
+
 */ 
