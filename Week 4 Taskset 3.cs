921fd6417f93d215d6450c48232a783399c73604
Ray <57572379+JustCallMeRay@users.noreply.github.com>
2021-11-02 19:22:24 +0000

    Update Week 4 Taskset 3.cs

--- Week 4 Taskset 3.cs
+++ Week 4 Taskset 3.cs
@@ -1,11 +1,11 @@
 /* 
-1 extend playground 	✓
-2 Create 3 puzzles 	✓
-3 Each puzzle blocks progression 	✓
+1 extend playground
+2 Create 3 puzzles	
+3 Each puzzle blocks progression
 4 progress to another level
 
 Extras: 
 Create a makeshift menu (on clicked events)
 Make a combination lock
-import assests, add lighting 	✓
+import assests, add lighting 	
 */ 
