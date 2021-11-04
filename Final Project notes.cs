43ddb4de2340687feeba9d3ddfa3c143446830aa
Ray <57572379+JustCallMeRay@users.noreply.github.com>
2021-11-04 19:09:11 +0000

    Create Final Project notes.cs

---
+++ Final Project notes.cs
@@ -0,0 +1,29 @@
+Coursework To do list
+	
+	Brief
+	{
+		proc gen world
+		{
+				Biomes
+				{
+					Blendig between biomes
+					Different enimes/monsters/ect
+					Biomes with floating islands
+				}
+			variation of z plane
+			Landscape shader based on angles of mesh. 
+			Caves????
+		}
+	}	
+	How to	
+	{
+		Compute shader	
+		{
+			large vorinoi noise for biome types. 		//How to deal with black spaces left by vorinoi? 
+			
+			//colour ID for each biome					//as more than 4 Biomes will be used
+			Doesn't work as gradients ruin colour ID system, 
+				channel bool system allows for 10 biomes.
+				or 6 with alpha. 
+		}
+	}
