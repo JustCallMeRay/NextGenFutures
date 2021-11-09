a52110cf90b7fd6d27fc19d86b1032d5e18809bb
Ray <57572379+JustCallMeRay@users.noreply.github.com>
2021-11-09 19:27:38 +0000

    Create Week 6 Taskset 1.cs

---
+++ Week 6 Taskset 1.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class Room1WallText : MonoBehaviour
+{
+    public TextMesh deltaTimeOutput;
+    public TextMesh fixedDeltaTimeOutput;
+    public TextMesh unscaledDeltaTimeOutput;
+    public TextMesh frameCountOutput;
+    public TextMesh fps;
+    public TextMesh Currentfps;
+    int framecountCurrent; 
+    int framecountLast = 0; 
+
+    void Update()
+    {
+        framecountCurrent = Time.frameCount;
+        int deltaframecount = framecountCurrent - framecountLast;
+        deltaTimeOutput.text = Time.deltaTime.ToString();
+        fixedDeltaTimeOutput.text = Time.fixedDeltaTime.ToString();
+        unscaledDeltaTimeOutput.text = Time.unscaledDeltaTime.ToString();
+        
+        //*I wasn't thinking :/   */
+        frameCountOutput.text = Time.frameCount.ToString();
+        Currentfps.text = (deltaframecount/Time.unscaledDeltaTime).ToString(); //(deltaframecount/Time.deltaTime).ToString();
+        fps.text = (Time.frameCount/ Time.time).ToString();
+        framecountLast = Time.frameCount;
+       
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+	    if (other.CompareTag("Player"))
+	    {
+		    Time.timeScale = 0.5f;
+	    }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+	    if (other.CompareTag("Player"))
+	    {
+		    Time.timeScale = 1;
+	    }
+    }
+
+}
