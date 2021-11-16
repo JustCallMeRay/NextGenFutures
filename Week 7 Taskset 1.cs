1a37f3b635d4ffda1134fe424684197d4381eb86
Ray <57572379+JustCallMeRay@users.noreply.github.com>
2021-11-16 19:08:29 +0000

    Update Week 7 Taskset 1.cs

--- Week 7 Taskset 1.cs
+++ Week 7 Taskset 1.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/* using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,5 +11,25 @@ public class Mycoin : MonoBehaviour
         CoinFlipAnim.Play();
         material.SetInteger("Face", 1);
         return ((Mathf.RoundToInt(Random.value) == 1));
+	}
+}
+*/ 
+
+//AFTER DEBUGING (thanks Lukus and Ant)
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mycoin : MonoBehaviour
+{
+    public Animation CoinFlipAnim;
+    public Material material;
+    public bool flipCoin()
+    {
+        CoinFlipAnim.Play();
+        int rand = Mathf.RoundToInt(Random.value);
+        material.SetInteger("Face", rand);
+        return (rand == 1);
 	}
 }
