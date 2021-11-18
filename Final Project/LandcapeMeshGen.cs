080e709f8561d03b1bee8c123e25df880948945c
Ray <57572379+JustCallMeRay@users.noreply.github.com>
2021-11-18 10:32:29 +0000

    y

--- Final Project/LandcapeMeshGen.cs
+++ Final Project/LandcapeMeshGen.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class LandcapeMeshGen : MonoBehaviour
 {
-    public Vector2 chunkSize = new Vector2(1,1);
+    public Vector2 chunkSize = new Vector2(1, 1);
     public Vector3 chunkCenter = new Vector3(0.5f, 0, 0.5f);
-   
-    [SerializeField]    //this looks awfull, why not serialize it like a vec4[]?
+
+
+    [SerializeField]
     private Matrix4x4 pointTransform = new Matrix4x4();//cannot be simplifed to new()
-        
+    //this looks awfull, why not serialize it like a vec4[]?
+    
+    
     //matrix turns points into relvent points for computeshader
        //so bounds when => 0-1, then points => points transformed
 
@@ -17,39 +20,18 @@ public class LandcapeMeshGen : MonoBehaviour
 
     [SerializeField]
     private Vector3[] WS_points = new Vector3[9];   //Points in world space "_" for unity
-    private Vector3[] CSpoints = new Vector3[9];  //points in "computeshader space"
+    [SerializeField] //for debuging, unneeded so removed for performance.
+    private Vector3[] CSpoints = new Vector3[9]; //points in "computeshader space"
 
     
 	public Vector3[] WS_Points => WS_points;  //A poor man's readonly
 	public Vector3[] CS_Points => CSpoints;   //A poor man's readonly
 
-    void Start()
-    {
-        min = (new Vector2( chunkCenter.x, chunkCenter.y)- chunkSize / 2);
-      
-            // matrix scaling   This would look better in z up!
-        /*x*/pointTransform[0, 0] = 1 / chunkSize.x;
-        /*y*/pointTransform[1, 1] = 1; //make sure its not 0 ( p.y*1 => p )
-        /*z*/pointTransform[2, 2] = 1 / chunkSize.y;
-        /*w*/pointTransform[3, 3] = 1; //Must be 1 in a tranform (idk, maths) 
-        
-           // matrix translation    (movement)
-        /*x*/pointTransform[0, 3] = -min.x;
-        /*y*/pointTransform[1, 3] = 0;      // p.y + 0 => p.y   // -min.z;
-        /*z*/pointTransform[2, 3] = -min.y; 
-              //w      already set 
 
-        for (int i=0; i <8; i++)
-        {
-            CSpoints[i] = pointTransform.MultiplyPoint(WS_points[i]);
-		}
-    
-    }
 
 	private void OnDrawGizmosSelected()
 	{
-        min = (new Vector2(chunkCenter.x, chunkCenter.y) - chunkSize / 2);
-      
+        min = new Vector2(chunkCenter.x, chunkCenter.z) - (chunkSize / 2);
             // matrix scaling   This would look better in z up!
         /*x*/pointTransform[0, 0] = 1 / chunkSize.x;
         /*y*/pointTransform[1, 1] = 1; //make sure its not 0 ( p.y*1 => p )
@@ -57,30 +39,37 @@ public class LandcapeMeshGen : MonoBehaviour
         /*w*/pointTransform[3, 3] = 1; //Must be 1 in a tranform (idk, maths) 
         
            // matrix translation    (movement)
-        /*x*/pointTransform[0, 3] = -min.x;
+        /*x*/pointTransform[0, 3] = (min.x);
         /*y*/pointTransform[1, 3] = 0;      // p.y + 0 => p.y   // -min.z;
-        /*z*/pointTransform[2, 3] = -min.y; 
-        //      w      already set       (3,3)
+        /*z*/pointTransform[2, 3] = (min.y); 
+        //      w      already set       (3,3);
+    
 
-              //vector maths
-
-        for (int i=0; i <7; i++)
+        //      vector maths
+        for (int i=0; i<9 ; i++)
         {
-            CSpoints[i] = pointTransform.MultiplyPoint(WS_points[i]);
-		}
+            //CSpoints[i] = pointTransform.MultiplyPoint(WS_points[i]);
+            CSpoints[i] = pointTransform.MultiplyPoint(WS_points[i]); //faster
+            //print(i);
+        }
 
+        Gizmos.color = Color.black;
+        Gizmos.DrawCube(new Vector3(0.5f, 0, 0.5f), new Vector3(1, 0, 1));
+        
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawCube(chunkCenter, new Vector3(chunkSize.x, 0, chunkSize.y));
+        Gizmos.DrawSphere(chunkCenter, 0.05f);
 
-        Gizmos.color = Color.red;
 		foreach (Vector3 p in WS_points)
         {
+            Gizmos.color = Color.red;
             Gizmos.DrawSphere(p, 0.1f);
 		}
         
-        Gizmos.color = Color.green;
         foreach (Vector3 p in CSpoints)
         {
+            Gizmos.color = Color.green;
             Gizmos.DrawSphere(p, 0.1f);
 		}
 	}
-
 }
